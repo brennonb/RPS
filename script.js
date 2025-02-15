66ee97f1e53cdfd7a71067d04c0e55934560277e
brennonb <82538044+brennonb@users.noreply.github.com>
2025-02-15 02:34:08 -0600

    Added getComputerChoice func and logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let x = Math.random();
+
+    if (x >= 0 && x < .33) {
+        return "Rock";
+    }
+    else if (x > .33 && x < .66) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+
+}
+
+console.log(getComputerChoice());
+
